4520816f7062a198fc6614c7979fc756bce54f35
Sheikh Hamim Mahmud <hamim.mahmud@azolutionse.com>
2023-08-08 00:02:29 +0600

    ArrayList

---
+++ 14_ArrayListApp/14_ArrayListApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList nameList = new ArrayList();
+
+            nameList.Add("Hamim");
+            nameList.Add(1);
+            nameList.Add("Hamim");
+            nameList.Add("2");
+            nameList.Add("Mahmud");
+            nameList.Add('B');
+            int capacity = nameList.Capacity;
+            nameList.RemoveAt(5);
+            nameList.Remove("Hamim");
+            for (var i = 0; i<nameList.Count; i++)
+            {
+                Console.WriteLine(nameList[i]);
+            }
+            Console.WriteLine("Capacity:"+capacity);
+
+            Console.ReadKey();
+        }
+    }
+}
